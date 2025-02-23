5319e019f5f0f91268fb072e9a8ea2101923e52b
Thomas Altenbach <thomas.altenbach@legrand.com>
2025-02-23 20:33:14 +0100

    wip

--- sim/src/image.rs
+++ sim/src/image.rs
@@ -1754,9 +1754,8 @@ fn tralier_estimation(dev: &dyn Flash) -> usize {
 
 fn image_largest_trailer(dev: &dyn Flash) -> usize {
     let trailer = if Caps::OverwriteUpgrade.present() {
-        // This computation is incorrect, and we need to figure out the correct size.
-        // c::boot_status_sz(dev.align() as u32) as usize
-        16 + 4 * dev.align()
+        // magic + image-ok + copy-done + swap-info
+        c::boot_magic_sz() + 3 * c::boot_max_align()
     } else if Caps::SwapUsingOffset.present() ||  Caps::SwapUsingMove.present() {
         let sector_size = dev.sector_iter().next().unwrap().size as u32;
         align_up(c::boot_trailer_sz(dev.align() as u32), sector_size) as usize
@@ -1852,7 +1851,7 @@ fn install_image(flash: &mut SimMultiFlash, slots: &[SlotInfo], slot_ind: usize,
 
             // the overflow size is roughly estimated to work for all
             // configurations. It might be precise if tlv_len will be maked precise.
-            largest_img_sz + dev.align()*4
+            largest_img_sz + dev.align()*3
         }
     };
 
